f35571b7a65f16667b4c85d136dc998bf746e0ce
Ben Yang <ben.yang@utexas.edu>
2024-09-11 13:12:57 -0500

    move ui to frontend

--- front/app/record/page.tsx
+++ front/app/record/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { Button } from "@/front/components/ui/button";
 import { useState, useEffect, useRef } from "react";
 
 export default function Record() {
